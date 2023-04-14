ceb714dd7825c14c2893a3ec29c5f901bbd9032b
reza_talebi <reza.talebi0902@gmail.com>
2023-04-15 00:14:34 +0430

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,13 +4,17 @@ import DashboardLayout from '@/layout/dashboard'
 import { getSessionServer } from '@/utils/getSessionServer'
 import { redirect } from 'next/navigation'
 import { ROUTES } from '@/models/enums'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
 
 export const metadata: Metadata = {
   title: 'پنل کاربری',
 }
 
 export default async function Layout({ children }: IChildrenProps) {
-  const data = await getSessionServer()
+  const data = await getServerSession(authOptions)
+
+  console.log(data)
 
   // if (!data) redirect(ROUTES.login)
 
