eb6e62e8ebc6d9658313ed92e8d4714b97f0b2bf
reza_talebi <reza.talebi0902@gmail.com>
2023-04-15 17:18:17 +0430

    Update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import { useGetSession } from '@/hook'
 import { ROUTES } from '@/models/enums'
 import Login from '@/modules/login'
 import { getSessionServer } from '@/utils/getSessionServer'
@@ -10,6 +11,10 @@ export const metadata: Metadata = {
 }
 
 const Page = () => {
+  const { user } = useGetSession()
+
+  console.log(user)
+
   return <Login />
 }
 
